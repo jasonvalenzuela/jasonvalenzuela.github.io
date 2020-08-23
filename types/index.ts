cfb26db38d8e3c4b78d066c5a7580edc87d1ced3
Jason Valenzuela <jev4zs@virginia.edu>
2020-08-22 20:13:14 -0400

    removing unneeded interface

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,5 @@
 export type ContactType = "email" | "phone" | "linkedin" | "github";
 
-export interface Post {
-  body: string;
-  id: number;
-  title: string;
-}
-
 export interface ContactContent {
   contactType: ContactType;
   text: string;
